065184bea8338e159eaffd882b6ffd528ce69ea7
H4CK3R-01 <resiaknairolf@gmail.com>
2021-06-11 07:55:46 +0200

    Reformatted code

--- Webservice/mobileHandler.js
+++ Webservice/mobileHandler.js
@@ -1,11 +1,11 @@
 function isMobile() {
-
-    var mobileDeviceIndicator = 0;
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    let mobileDeviceIndicator = 0;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         mobileDeviceIndicator = 1;
     }
     /*
-    var hasTouchscreen = 'ontouchstart' in window;
-    alert(hasTouchscreen ? 'has touchscreen' : 'doesn\'t have touchscreen');*/
+    let hasTouchscreen = 'ontouchstart' in window;
+    alert(hasTouchscreen ? 'has touchscreen' : 'doesn\'t have touchscreen');
+    */
 }
 
--- public/js/Sprite.js
+++ public/js/Sprite.js
@@ -6,12 +6,12 @@ function Sprite(x, y) {
     this.getSprite = function () {
         this.setSize(this.sprite, sprite_size);
         return this.sprite;
-    }
+    };
 
     this.setSize = function (sprite, size) {
         sprite.x = this.coord_x * size - size * 0.2;
         sprite.y = this.coord_y * size - size * 0.2;
         sprite.width = size * 1.5;
         sprite.height = size * 1.5;
-    }
+    };
 }
