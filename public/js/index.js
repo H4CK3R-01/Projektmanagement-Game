9a37f674b31d3f4a8b336b3ae6d41678b4e536f4
H4CK3R-01 <resiaknairolf@gmail.com>
2021-06-18 13:18:25 +0200

    fixed duplicate messages

--- public/js/index.js
+++ public/js/index.js
@@ -14,8 +14,41 @@ document.getElementById('ok').addEventListener('click', function () {
         closeOnBeforeunload: false
     });
 
-    start_chat();
-
     // Login
     socket.emit('add user', {'username': username, 'room_name': room_name});
+
+
+    socket.on('login', function () {
+        connected = true;
+
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('game').style.display = 'flex';
+        document.getElementById('chat').style.display = 'flex';
+        start_game();
+        resize();
+
+        addLogMessage("Welcome " + username + "!");
+    });
+
+    socket.on('error', function (data) {
+        if (data === 'Game started already or room has two many members') {
+            document.getElementById('login').style.display = 'flex';
+            document.getElementById('game').style.display = 'none';
+            document.getElementById('chat').style.display = 'none';
+            document.getElementById('error').innerText = data;
+        }
+        console.log(data);
+    });
+
+    socket.on('new message', function (data) {
+        addChatMessage(data);
+    });
+
+    socket.on('user joined', function (data) {
+        addLogMessage(data + ' joined');
+    });
+
+    socket.on('user left', function (data) {
+        addLogMessage(data + ' left');
+    });
 });
