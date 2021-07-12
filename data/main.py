7ad1430ac621b843abe95b3f519b0341bb7e0b7c
H4CK3R-01 <44125287+H4CK3R-01@users.noreply.github.com>
2021-07-12 17:01:33 +0200

    Updated questions

--- data/main.py
+++ data/main.py
@@ -1,59 +1,19 @@
 import json
+import io
 
 if __name__ == '__main__':
     array = []
-    with open('fragen_10_06_21_final.json') as file:
+    with io.open('fragen_12_07_21.json', 'r', encoding='utf-8-sig') as file:
         data = json.load(file)
 
-        length_q = 0
-        id_q = 0
-        length = 0
-        id = 0
         for i in range(0, len(data)):
-            new = {'id': int(data[i]['id']), 'difficulty': int(data[i]['difficulty']), 'question': data[i]['question'],
-                   'answers': []}
+            new = { 'id': int(data[i]['ID']), 'difficulty': int(data[i]['Schwierigkeit']), 'question': data[i]['Frage'], 'answers': [] }
 
-            new['answers'].append({"text": data[i]['A'],
-                                   "status": 'A' == data[i]['key']
-                                   })
-            new['answers'].append({"text": data[i]['B'],
-                                   "status": 'B' == data[i]['key']
-                                   })
-            new['answers'].append({"text": data[i]['C'],
-                                   "status": 'C' == data[i]['key']
-                                   })
-            new['answers'].append({"text": data[i]['D'],
-                                   "status": 'D' == data[i]['key']
-                                   })
-
-
-            if(length_q < len(data[i]['question'])):
-                length_q = len(data[i]['question'])
-
-
-            if(length < len(data[i]['A'])):
-                length = len(data[i]['A'])
-            if(length < len(data[i]['B'])):
-                length = len(data[i]['B'])
-            if(length < len(data[i]['C'])):
-                length = len(data[i]['C'])
-            if(length < len(data[i]['D'])):
-                length = len(data[i]['D'])
-
+            new['answers'].append({"text": data[i]['A'], "status": 'A' == data[i]['Key']})
+            new['answers'].append({"text": data[i]['B'], "status": 'B' == data[i]['Key']})
+            new['answers'].append({"text": data[i]['C'], "status": 'C' == data[i]['Key']})
+            new['answers'].append({"text": data[i]['D'], "status": 'D' == data[i]['Key']})
             array.append(new)
 
-    print("Längste Frage: " + str(length))
-    print("Längste Antwort: " + str(length_q))
-
-    with open('fragen_10_06_21_final.json') as file:
-        data = json.load(file)
-
-        for i in range(0, len(data)):
-            if(length_q == len(data[i]['question'])):
-                print("Längste Frage ID: " + str(data[i]['id']))
-
-            if(length == len(data[i]['A']) or length == len(data[i]['B']) or length == len(data[i]['C']) or length == len(data[i]['D'])):
-                print("Längste Antwort ID: " + str(data[i]['id']))
-
-    with open('fragen_10_06_21_final_new_format.json', 'w', encoding='utf8') as file:
+    with open('fragen_12_07_21_new_format.json', 'w', encoding='utf8') as file:
         json.dump(array, file, ensure_ascii=False)
